df9429030cd2dd89602f736d77e9eaac3ae2437f
Folkert Stijnman <folkertstijnman@gmail.com>
2025-01-20 20:19:22 +0100

    regression

--- playground_s05e01_forecasting_sticker_sales/src/forecast_sticker_sales/main.py
+++ playground_s05e01_forecasting_sticker_sales/src/forecast_sticker_sales/main.py
@@ -1,8 +1,23 @@
 from loguru import logger
+import pandas as pd
+
+
+def load_data():
+    logger.info("Loading data")
+    data = pd.read_csv("data/train.csv")
+    logger.info(f"Data loaded: {data.head()}")
+    return
+
+
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    logger.info("Transforming data")
+
+    return
 
 
 def main():
     logger.info("Forecasting sticker sales")
+    load_data()
     return
 
 
